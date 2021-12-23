7764de9a570bcefd59ee163d6c49b4517dd90ae9
y-solb <ysolb99@gmail.com>
2021-12-23 17:02:47 +0900

    feat: implement card deletion

--- src/components/cardItem.jsx
+++ src/components/cardItem.jsx
@@ -7,8 +7,16 @@ import hat from "../assets/imgs/hat.png";
 import rudolf from "../assets/imgs/rudolf.png";
 import sled from "../assets/imgs/sled.png";
 import socks from "../assets/imgs/socks.png";
+import bin from "../assets/imgs/bin.png";
+import { useDispatch } from "react-redux";
+import { deleteCard } from "../modules/cardSlice";
 
 const CardItem = ({ id, deco, title, content, top, left }) => {
+  const dispatch = useDispatch();
+  const onDelete = () => {
+    dispatch(deleteCard({ id: id }));
+  };
+
   return (
     <Popup
       trigger={
@@ -20,6 +28,9 @@ const CardItem = ({ id, deco, title, content, top, left }) => {
       <CardBox>
         <h3>{title}</h3>
         <span>{content}</span>
+        <DeleteBtn onClick={onDelete}>
+          <DeleteImg src={bin} />
+        </DeleteBtn>
       </CardBox>
     </Popup>
   );
@@ -65,6 +76,25 @@ const CardBox = styled.div`
   padding: 30px 30px 60px 30px;
   background-color: white;
   border-radius: 8px;
+`;
+
+const DeleteBtn = styled.button`
+  position: absolute;
+  bottom: 18px;
+  right: 20px;
+  width: 24px;
+  height: 24px;
+  border: none;
+  background: none;
+  &:hover {
+    transition: transform 100mx ease-in;
+    transform: scale(1.1);
+  }
+`;
+
+const DeleteImg = styled.img`
+  width: 24px;
+  height: 24px;
 `;
 
 export default CardItem;
--- src/modules/cardSlice.jsx
+++ src/modules/cardSlice.jsx
@@ -11,9 +11,15 @@ const cardSlice = createSlice({
     addCard: (state, action) => {
       state.cardList.push(action.payload);
     },
+    deleteCard: (state, action) => {
+      state.cardList.splice(
+        state.cardList.findIndex((card) => card.id === action.payload),
+        1
+      );
+    },
   },
 });
 
-export const { addCard } = cardSlice.actions;
+export const { addCard, deleteCard } = cardSlice.actions;
 export const selectCardList = (state) => state.cards.cardList;
 export default cardSlice.reducer;
